39e13ad858c063d7d5de1cb2372cd1c3d663f0b4
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-04-24 20:05:33 +0200

    "Conexión con BBDD establecida con éxito" "Servidor escuchando con éxito"

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const { checkConnection, syncModels } = require('./db/index.db.js')
+//const addRelationsToModels = require('./db/models')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+//const router = require('./api/routes/index.js')
+
+
+async function dbConnect() {
+
+    try {
+        await checkConnection()
+        //addRelationsToModels()
+        //await syncModels('alter') 
+    } catch (error) {
+        console.log('Something has gone very wrong 😱', error)  
+    }
+}
+
+
+const app = express()
+const port = process.env.PORT
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors())
+
+
+app.listen(port, async () => {
+    try {
+        await dbConnect()
+        console.log(`Server is listening in port ${port}`)
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//app.use('/api', router)

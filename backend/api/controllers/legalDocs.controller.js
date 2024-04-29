ea9bf28cc843bf9af34699d7e81e18762dbe1a1f
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-04-29 21:49:25 +0200

    Función de update y cambio de estado de review hecha y funcional

--- backend/api/controllers/legalDocs.controller.js
+++ backend/api/controllers/legalDocs.controller.js
@@ -1,6 +1,5 @@
-const LegalDoc = require('../models/legalDoc.model')
-const Review = require('../models/review.model');
-
+const LegalDoc = require("../models/legalDoc.model");
+const Review = require("../models/review.model");
 
 // Create legalDoc Admin y user
 const createLegalDoc = async (req, res) => {
@@ -9,7 +8,7 @@ const createLegalDoc = async (req, res) => {
       document: req.body.document,
       attributes: {
         exclude: ["status"],
-      }
+      },
     });
     delete req.body.status;
     return res.status(200).json(legalDoc);
@@ -21,15 +20,16 @@ const createLegalDoc = async (req, res) => {
 
 // Gel All LegalDocs - Admin
 const getAllLegalDocs = async (req, res) => {
-    try {
-      const docs = await LegalDoc.findAll({
-      });
-      return res.status(200).json({message: "Here are all the legal docs", docs});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Something went wrong" });
-    }
-  };
+  try {
+    const docs = await LegalDoc.findAll({});
+    return res
+      .status(200)
+      .json({ message: "Here are all the legal docs", docs });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 // Gel One LegalDoc - Admin
 async function getOneLegalDoc(req, res) {
@@ -45,45 +45,43 @@ async function getOneLegalDoc(req, res) {
   }
 }
 
-//Update LegalDoc (admin)
+//updateLegalDoc - Admin
+
 
 async function updateLegalDoc(req, res) {
-	try {
-		const legalDoc = await LegalDoc.update(req.body, {
-			returning: true,
-			where: {
-				id: req.params.legalDocId,
-			},
-		})
-		if (legalDoc !== 0) {
-			return res.status(200).json({ message: 'Legal Docs updated', legalDoc })
-		} else {
-			return res.status(404).send('Legal doc not found')
-		}
-	} catch (error) {
-		return res.status(500).send(error.message)
-	}
-}
-/*
-// Delete one legal Doc - Admin
-const deleteLegalDoc = async (req, res) => {
   try {
-    const legalDoc = await LegalDoc.destroy({
+    const [rowCount] = await LegalDoc.update(req.body, {
       where: {
         id: req.params.legalDocId,
       },
     });
-    if (legalDoc > 0) {
-      return res.status(200).json("LegalDoc deleted");
+
+    if (rowCount > 0) {
+      const updatedLegalDoc = await LegalDoc.findByPk(req.params.legalDocId);
+      
+      if (updatedLegalDoc.status === "aceptado") {
+
+        const findReviews = await Review.findAll({
+          where: {
+            legalDocId: req.params.legalDocId,
+          }
+        });
+
+        await Promise.all(findReviews.map(async (review) => {
+          await review.update({ postedStatus: 'yes' });
+        }));
+      }
+
+      console.log(updatedLegalDoc);
+      return res.status(200).json({ message: "Legal Doc updated", legalDoc: updatedLegalDoc });
     } else {
-      return res.status(404).send("LegalDoc not found");
+      return res.status(404).send("Legal doc not found");
     }
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Something went wrong" });
+    return res.status(500).send(error.message);
   }
-};
-*/
+}
+
 
 // Método para eliminar un LegalDoc y su Review asociada
 const deleteLegalDoc = async (req, res) => {
@@ -106,17 +104,19 @@ const deleteLegalDoc = async (req, res) => {
     // Eliminar el LegalDoc
     await legalDoc.destroy();
 
-    return res.status(200).json({ message: "LegalDoc and associated Review deleted successfully" });
+    return res
+      .status(200)
+      .json({ message: "LegalDoc and associated Review deleted successfully" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
 
-  module.exports ={
-    getAllLegalDocs,
-    createLegalDoc,
-    getOneLegalDoc,
-    updateLegalDoc,
-    deleteLegalDoc
-  }
+module.exports = {
+  getAllLegalDocs,
+  createLegalDoc,
+  getOneLegalDoc,
+  updateLegalDoc,
+  deleteLegalDoc,
+};

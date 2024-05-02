660af30b75c6bb2a9ab6df8ea6ad211f8c448bff
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-05-02 19:33:12 +0200

    Usuario puede acceder a sus datos de perfil Home de usuario

---
+++ frontend/my-react-app/src/services/user.service.js
@@ -0,0 +1,41 @@
+import api from "./config.service"
+
+
+export const getOwnProfile = async (token) => {
+  
+    try {
+      const { data } = await api.get('/users/profile', {
+        headers: {
+          Authorization: token,
+        },
+      });
+      console.log(data)
+      return data;
+    } catch (error) {
+      console.log('Error getting own profile: ', error.message);
+      throw error;
+    }
+  };
+  
+
+  export const UpdateOwnProfile = async (name, surname, email, dni, phone, token) => {
+    try {
+      const { data } = await api.post('/users/profile', {
+        name: name,
+        surname: surname,
+        email: email,
+        dni: dni, 
+        phone: phone
+      }, {
+        headers: {
+          Authorization: token
+        }
+      });
+  
+      return data;
+    } catch (error) {
+      console.log('Error updating own profile: ', error.message);
+      throw error;
+    }
+  };
+  

effcb6662a9fcacc502e6d272c93240bac35e36d
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-05-04 18:17:33 +0200

    user Home Modificado

--- frontend/my-react-app/src/pages/UserHome/UserHome.jsx
+++ frontend/my-react-app/src/pages/UserHome/UserHome.jsx
@@ -5,31 +5,26 @@ import "./UserHome.css"
 import { useNavigate } from 'react-router-dom';
 
 
-
 function UserHome() {
   const navigate = useNavigate();
 
   const handleReviewClick = () => {
-
     navigate('/user/reviews');
   };
 
-  const handleLegalDocsClick = () => {
-
-    navigate('/user/legaldocs');
-  };
-
   const handleProfileClick = () => {
-
     navigate('/user/profile');
   };
 
+  const handleLogoutClick = () => {
 
+    navigate('/login');
+  };
 
   return (
     <>
       <NavBar />
-      <Container className="mt-5 contenedor" >
+      <Container className="mt-5 contenedor">
         <div className="border p-4 text-center">
           <h2>Tus Datos de Perfil</h2>
         </div>
@@ -37,32 +32,28 @@ function UserHome() {
           <Row className="justify-content-center contenedorbotones">
             <Col xs={12} md={6}>
               <Button 
-              variant="primary" 
-              size="lg" 
-              className="mr-md-2 mb-3 mb-md-0 boton"
-              onClick={handleReviewClick}
+                variant="primary" 
+                size="lg" 
+                className="mr-md-3 mb-3 boton"
+                onClick={handleReviewClick}
               >
                 Mis Reseñas
               </Button>
               <Button 
-              variant="primary" 
-              size="lg" 
-              className="mb-3 mb-md-0 boton"
-              onClick={handleLegalDocsClick}
-              >
-                Mis LegalDocs
-              </Button>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs={12} md={6}>
-              <Button 
-              variant="primary" 
-              size="lg" 
-              className="mb-3 boton"
-              onClick={handleProfileClick}
+                variant="primary" 
+                size="lg" 
+                className="mr-md-3 mb-3 boton"
+                onClick={handleProfileClick}
               >
                 Datos Personales
+              </Button>
+              <Button 
+                variant="primary" 
+                size="lg" 
+                className="mb-3 boton"
+                onClick={handleLogoutClick}
+              >
+                Cerrar Sesión
               </Button>
             </Col>
           </Row>

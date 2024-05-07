fd814f86e4bc30b6723e1a5b1134f4328ef35bf2
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-05-07 20:01:53 +0200

    Botón de ver más ya redirige

--- frontend/my-react-app/src/components/Carrusel/Carrusel.jsx
+++ frontend/my-react-app/src/components/Carrusel/Carrusel.jsx
@@ -2,7 +2,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import imagen1 from '../../assets/img/banner1.jpg';
 import imagen2 from '../../assets/img/banner2.jpg';
 import imagen3 from '../../assets/img/banner3.jpg';
+import { Link } from 'react-router-dom';
 import './Carrusel.css'
+
 
 function Carrusel() {
     return (
@@ -20,7 +22,9 @@ function Carrusel() {
                         <h3 className='fw-bolder mb-4'>Aquí SÍ hay quien viva</h3>
                         <p className=''>¿Te vas a vivir a un <strong>piso nuevo</strong> en Madrid?</p>
                         <p className=''>Comprueba nuestras <strong>reseñas</strong> y evita sorpresas.</p>
+                        <Link to={"/about"}>
                         <button type='button' className='btn boton-marron'>VER MÁS</button>
+                        </Link>
                     </div>
                 </div>
             </Carousel.Caption>
@@ -38,7 +42,9 @@ function Carrusel() {
                         <h3 className='fw-bolder texto-sg mb-4'>¿Te lo estas pensando?</h3>
                         <p className='texto-sg'>Resuelve tus dudas antes de mudarte</p>
                         <p className='texto-sg'>Nuestros usuarios te cuentan sus experiencias en pisos de Madrid</p>
+                        <Link to={"/about"}>
                         <button type='button' className='btn boton-amarillo'>VER MÁS</button>
+                        </Link>
                     </div>
                 </div>
             </Carousel.Caption>
@@ -56,12 +62,13 @@ function Carrusel() {
                         <h3 className='fw-bolder texto-tr'>¿Quieres contarnos tu experiencia viviendo en Madrid?</h3>
                         <p className='texto-tr'>Registrate en nuestra web y añade tu reseña</p>
                         <p className='texto-tr'>Ayuda a otros usuarios a tomar la decisión</p>
+                        <Link to={"/about"}>
                         <button type='button' className='btn boton-blanco'>VER MÁS</button>
+                        </Link>
                     </div>
                 </div>
             </Carousel.Caption>
         </Carousel.Item>
-        {/* Resto de los items del carrusel */}
     </Carousel>
 );
 }

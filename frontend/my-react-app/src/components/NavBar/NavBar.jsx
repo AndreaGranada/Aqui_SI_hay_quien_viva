fb9e91271d26859969c6cddf000b998c44d531ed
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-05-07 11:52:28 +0200

    Logo NavBar lleva a la home

--- frontend/my-react-app/src/components/NavBar/NavBar.jsx
+++ frontend/my-react-app/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css'
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container';
-import { Nav } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Image } from 'react-bootstrap';
-import imagen_logo from '../../assets/img/logo_amarillo.png';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import { Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Image } from "react-bootstrap";
+import imagen_logo from "../../assets/img/logo_amarillo.png";
 
 function NavBar() {
   const [roleUser, setRoleUser] = useState("");
@@ -35,16 +34,37 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-naranja fs-5">
       <Container>
-        <Navbar.Brand href="#home" className='fs-2'>
-          <Image className='navbar_logo' src={imagen_logo} />
+        <Navbar.Brand as={Link} to="/" className="fs-2">
+          <Image className="navbar_logo" src={imagen_logo} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <div className="justify-content-end">
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link><Link to="/" className="nav-link" style={{ textDecoration: 'none', color: '#F2E963' }}>Home</Link></Nav.Link>
-              <Nav.Link><Link to="/about" className="nav-link" style={{ textDecoration: 'none', color: '#F2E963' }}>Sobre Nosotros</Link></Nav.Link>
-              <NavDropdown title="Mi perfil" className="nav-link" id="basic-nav-dropdown" style={{ textDecoration: 'none', color: '#403529' }}>
+              <Nav.Link>
+                <Link
+                  to="/"
+                  className="nav-link"
+                  style={{ textDecoration: "none", color: "#F2E963" }}
+                >
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link
+                  to="/about"
+                  className="nav-link"
+                  style={{ textDecoration: "none", color: "#F2E963" }}
+                >
+                  Sobre Nosotros
+                </Link>
+              </Nav.Link>
+              <NavDropdown
+                title="Mi perfil"
+                className="nav-link"
+                id="basic-nav-dropdown"
+                style={{ textDecoration: "none", color: "#403529" }}
+              >
                 <NavDropdown.Item></NavDropdown.Item>
                 <NavDropdown.Item>
                   <Link to="/signup">Registrate</Link>
